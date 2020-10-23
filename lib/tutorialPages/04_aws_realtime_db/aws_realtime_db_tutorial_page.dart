654b04e7ed43d1b5293a286bf9889d6a8e24b319
Taehoon <hoonblizz@gmail.com>
2020-10-23 01:13:40 -0400

    basic template

---
+++ lib/tutorialPages/04_aws_realtime_db/aws_realtime_db_tutorial_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AWSRealtimeDBTutorialPage extends StatefulWidget {
+  @override
+  _AWSRealtimeDBTutorialPageState createState() =>
+      _AWSRealtimeDBTutorialPageState();
+}
+
+class _AWSRealtimeDBTutorialPageState extends State<AWSRealtimeDBTutorialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
